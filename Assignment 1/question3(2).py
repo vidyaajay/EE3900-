58b7b22f9ad2cc2ff911860942b1fdd4eed58057
vidyaajay <108403108+vidyaajay@users.noreply.github.com>
2022-08-18 10:24:38 +0530

    Update question3(2).py

--- Assignment 1/question3(2).py
+++ Assignment 1/question3(2).py
@@ -1,10 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#If using termux
-import subprocess
-import shlex
-#end if
-
 
 
 x=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
@@ -36,11 +31,5 @@ plt.subplot(2, 1, 2)
 plt.stem(range(0,k),y)
 plt.xlabel('$n$')
 plt.ylabel('$y(n)$')
-plt.grid()# minor
-
-#If using termux
-#plt.savefig('../figs/xnyn.pdf')
-#plt.savefig('../figs/xnyn.eps')
-#subprocess.run(shlex.split("termux-open ../figs/xnyn.pdf"))
-#else
+plt.grid()
 plt.show()
