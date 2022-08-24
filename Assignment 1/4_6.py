1e4cd68b964334a38ce9d7d8d3b850037a5c2d20
vidyaajay <108403108+vidyaajay@users.noreply.github.com>
2022-08-24 10:44:54 +0530

    Create 4_6.py

---
+++ Assignment 1/4_6.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def H(z):
+	num = np.polyval([1,0,1],z**(-1))
+	den = np.polyval([0.5,1],z**(-1))
+	H = num/den
+	return H
+		
+
+
+#Input and Output
+omega = np.linspace(-3*np.pi,3*np.pi,100)
+
+#subplots
+plt.plot(omega, abs(H(np.exp(1j*omega))))
+plt.title('Filter Frequency Response')
+plt.xlabel('$\omega$')
+plt.ylabel('$|H(e^{\jmath\omega})| $')
+plt.grid()
+
+
+plt.show()
+
+
+
